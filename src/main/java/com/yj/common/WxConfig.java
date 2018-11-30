a2a067a91e1e96122aa4044209f30423770ce5bc
666666 <632543040@qq.com>
2018-11-30 15:51:13 +0800

    2018/11/30 15:51

--- src/main/java/com/yj/common/WxConfig.java
+++ src/main/java/com/yj/common/WxConfig.java
@@ -1,7 +1,7 @@
 package com.yj.common;
 
 public class WxConfig {
-    public static final String wx_app_id = "wx05275af36fbf9194";                //微信小程序app id
-    public static final String wx_app_secret = "710eee1cc6c72ceee26ee5c2c3b1f142";   //微信小程序app secret
+    public static final String wx_app_id = "wx915c7b2ebc140ee6";                //微信小程序app id
+    public static final String wx_app_secret = "535d043903b6e6c08538c3fcf5e2c570";   //微信小程序app secret
     public static final String wx_login_url = "https://api.weixin.qq.com/sns/jscode2session";   //登录地址
 }
