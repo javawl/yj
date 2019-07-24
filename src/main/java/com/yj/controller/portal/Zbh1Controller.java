dc083577b1f922925cb10f26fd6bd1011c674594
666666 <632543040@qq.com>
2019-07-24 18:36:04 +0800

    2019/7/24

--- src/main/java/com/yj/controller/portal/Zbh1Controller.java
+++ src/main/java/com/yj/controller/portal/Zbh1Controller.java
@@ -10,9 +10,6 @@ import com.yj.service.IFileService;
 import com.yj.service.ITokenService;
 import com.yj.service.IVariousService;
 import com.yj.service.IZbh1Service;
-import oracle.jrockit.jfr.events.RequestableEventEnvironment;
-import org.apache.http.client.methods.HttpOptions;
-import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +21,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.RequestWrapper;
-import java.lang.ref.ReferenceQueue;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 @Controller
